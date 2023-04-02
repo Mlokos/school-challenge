16952b0a81ac8341a6b47ed82e5478ce22bbbd66
mlokos <mlokos.github@gmail.com>
2023-04-02 23:22:03 +0200

    check macOS vm runner

--- lib/school_challenge/password_cracker_challenge.ex
+++ lib/school_challenge/password_cracker_challenge.ex
@@ -6,21 +6,29 @@ defmodule PasswordCrackerChallenge do
 
   TIP: należy zwrócić uwagę, czy apostrofy są '' (lista znaków) czy "" (String, binary)
   """
+  @chunk_size 24_000_000
+  @parallelizaiton 6
+
   def guess_password(hash), do: guess_password(hash, 0)
-  def guess_password(_hash, 10), do: raise "Password not found"
   def guess_password(hash, n) do
-    trunc(:math.pow(26, n)) - 1..trunc(:math.pow(26, n + 1)) - 1
-    |> Flow.from_enumerable()
-    |> Flow.map(&check_number(&1, hash))
-    |> Flow.filter(fn
-      {true, _} -> true
-      _ -> false
-    end)
-    |> Enum.to_list()
+    n * @chunk_size..(n + 1) * @chunk_size
+    |> Enum.chunk_every(div(@chunk_size, @parallelizaiton))
+    |> Enum.map(&(Task.async(fn -> check_list_of_numbers(&1, hash) end)))
+    |> Enum.map(fn x -> Task.await(x, 30_000) end)
+    |> List.flatten()
     |> case do
       [] -> guess_password(hash, n + 1)
       [{true, x}] -> x
     end
+  end
+
+  defp check_list_of_numbers(numbers, hash) do
+    numbers
+    |> Enum.map(&check_number(&1, hash))
+    |> Enum.filter(fn
+      {true, _} -> true
+      _ -> false
+    end)
   end
 
   defp check_number(number, hash) do
@@ -34,8 +42,7 @@ defmodule PasswordCrackerChallenge do
   defp number_to_password(number, password) do
     number
     |> rem(26)
-    |> (fn(x) -> [x + 97] end).()
-    |> (fn(x) -> number_to_password(div(number, 26), x ++ password) end).()
+    |> (fn(x) -> number_to_password(div(number, 26), [x + 97] ++ password) end).()
   end
 
   defp check_password(password, actual_password_hash) do
