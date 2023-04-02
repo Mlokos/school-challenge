f1b8b84b4c611abd745378706eca435a82f7cd28
mlokos <mlokos.github@gmail.com>
2023-04-02 03:11:18 +0200

    create setup for default github runner

--- lib/school_challenge/password_cracker_challenge.ex
+++ lib/school_challenge/password_cracker_challenge.ex
@@ -6,26 +6,20 @@ defmodule PasswordCrackerChallenge do
 
   TIP: należy zwrócić uwagę, czy apostrofy są '' (lista znaków) czy "" (String, binary)
   """
-  @chunk_size 320_000
-  @workload 5_000
+  @chunk_size 3_000_000
 
   def guess_password(hash), do: guess_password(hash, 0)
   def guess_password(hash, n) do
-    :erlang.garbage_collect()
-
-    result = n * @chunk_size..(n + 1) * @chunk_size
-    |> Enum.chunk_every(@workload)
+    n * @chunk_size..(n + 1) * @chunk_size
+    |> Enum.chunk_every(div(@chunk_size, 2))
     |> Enum.map(&(Task.async(fn -> check_list_of_numbers(&1, hash) end)))
     |> Enum.map(&Task.await/1)
     |> List.flatten()
     |> Enum.filter(&match?({true, _}, &1))
-
-    if result == [] do
-      guess_password(hash, n + 1)
-    else
-      Enum.at(result, 0)
-      |> elem(1)
-    end
+    |> case do
+        [] -> guess_password(hash, n + 1)
+        [true: x] -> x
+      end
   end
 
   defp check_list_of_numbers(numbers, hash) do
@@ -42,15 +36,13 @@ defmodule PasswordCrackerChallenge do
   defp number_to_password(0, ''), do: 'a'
   defp number_to_password(0, password), do: password
   defp number_to_password(number, password) do
-    char = number
+    number
     |> rem(26)
     |> (fn(x) -> [x + 97] end).()
-
-    number_to_password(div(number, 26), char ++ password)
+    |> (fn(x) -> number_to_password(div(number, 26), x ++ password) end).()
   end
 
   defp check_password(password, actual_password_hash) do
-    guessed_password_hash = :crypto.hash(:sha512, password)
-    {actual_password_hash == guessed_password_hash, password}
+    {actual_password_hash == :crypto.hash(:sha512, password), password}
   end
 end
